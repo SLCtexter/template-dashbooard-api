e10df6d7e91a80b936fcd575a14aae704768f1c0
SLCtexter <chamara.a.amarasinghe@gmail.com>
2026-05-21 14:16:40 +0530

    Fix listen on 0.0.0.0

--- server.js
+++ server.js
@@ -235,4 +235,4 @@ app.patch('/api/admin/verify-bank-order/:orderUuid', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
